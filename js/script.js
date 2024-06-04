f59ab5d64309fc6675677de1de3590a0583b87c7
kama <165862933+7osin@users.noreply.github.com>
2024-06-04 13:30:32 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function toggle() {
-    const toggle = document.querySelector(".toggle");
-    const banner = document.querySelector(".banner");
-    toggle.classList.toggle("active");
-    banner.classList.toggle("active");
-}
