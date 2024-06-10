1192b6e35550a668d42693c2f99ba96b6afd4e4f
kama <165862933+7osin@users.noreply.github.com>
2024-06-10 17:48:24 +0300

    Add files via upload

---
+++ js/hwid.js
@@ -0,0 +1,35 @@
+function showNotification(message, type) {
+  const notification = document.getElementById('notification');
+  notification.innerText = message;
+  notification.className = `notification ${type}`;
+  notification.style.display = 'block';
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 5000);
+}
+
+function ResetKey() {
+  const key = document.getElementById('name').value;
+  const version = document.querySelector('input[name="version"]:checked').value;
+
+  const url = `https://peacedeath.su/hwid/source/KAMA/reset.php?k=${encodeURIComponent(key)}&v=${encodeURIComponent(version)}`;
+
+  fetch(url)
+    .then(response => {
+      if (response.status === 200) {
+        return response.text().then(data => {
+          showNotification(data, 'success');
+        });
+      } else if (response.status === 400) {
+        return response.text().then(data => {
+          showNotification(data, 'error');
+        });
+      } else {
+        throw new Error('Unexpected response status');
+      }
+    })
+    .catch(error => {
+      showNotification(`An error occurred: ${error.message}`, 'error');
+    });
+}
